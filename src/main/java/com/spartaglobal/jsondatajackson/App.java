a8df36358dadbf9d3d758fd21d988b9d2fa2c0ea
Rahul Rajeev <RRajeev@spartaglobal.com>
2019-06-12 12:05:40 +0100

    added everthing

--- src/main/java/com/spartaglobal/jsondatajackson/App.java
+++ src/main/java/com/spartaglobal/jsondatajackson/App.java
@@ -1,13 +1,19 @@
 package com.spartaglobal.jsondatajackson;
 
+import JacksonDeserialisation.RatesDeserialiser;
+import JacksonManualParsing.JacksonManualExample;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    JacksonManualExample manualExample = new JacksonManualExample("resources/rates.json");
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JacksonManualExample manualExample = new JacksonManualExample("resources/rates.json");
+        RatesDeserialiser ratesDeserialiser = new RatesDeserialiser("resources/rates.json");
+        System.out.println( ratesDeserialiser.ratesMapped.getDate() );
     }
 }
