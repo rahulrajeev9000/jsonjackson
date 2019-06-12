502cbc8324cc1fa2597f01afc2a5723fc83e22b2
Rahul Rajeev <RRajeev@spartaglobal.com>
2019-06-12 11:01:40 +0100

    add ignore, pom, resources, src

---
+++ src/main/java/com/spartaglobal/jsondatajackson/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.jsondatajackson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
